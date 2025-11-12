3656f831eee719383be3f98753d4a4669b28a32a
Tsunami43 <n3m0_22@mail.ru>
2025-11-12 10:15:00 +0300

    Add menu bar application structure

---
+++ MenuBarCaptureApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct MenuBarCaptureApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    var body: some Scene {
+        Settings {
+            EmptyView()
+        }
+    }
+}
